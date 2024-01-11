e37e56f6a6e0b48715cae6c74795377c8abde944
jinqiqing <389459119@qq.com>
2024-01-11 17:50:10 +0800

    增加了一个popular项目的list（暂不完全），并可以从这个list中爬

--- backend/src/main/java/nju/edu/cn/qysca/utils/spider/Spider.java
+++ backend/src/main/java/nju/edu/cn/qysca/utils/spider/Spider.java
@@ -11,39 +11,22 @@ import java.util.Arrays;
 import java.util.List;
 
 public class Spider {
-
-    private static final String DEFAULT_DOWNLOAD_PATH = "backend/src/main/resources/poms/";
     private static final String DEFAULT_TYPE = "pom";
     private static final String DEFAULT_URL_LIST_PATH = "backend/src/main/resources/url_list.txt";
 
     public static void main(String[] args) {
 
-//        String downloadPath = DEFAULT_DOWNLOAD_PATH;
-//        Boolean isWriteCsv = true;
-//        String type;
-//
-//        System.out.println("Command-line arguments: " + Arrays.toString(args));
-//        // 解析args参数
-//        for (int i = 0; i < args.length; i++) {
-//            if (args[i].equalsIgnoreCase("downloadPath") && i + 1 < args.length) {
-//                downloadPath = args[i + 1];
-//                System.out.println("Download Path: " + downloadPath);
-//                i++;
-//            } else if (args[i].equalsIgnoreCase("writeCsv")) {
-//                isWriteCsv = true;
-//            } else if (args[i].equalsIgnoreCase("pom")) {
-//                type = "pom";
-//            }
-//        }
+        List<String> directoryUrlList = readLinesFromFile(DEFAULT_URL_LIST_PATH);
+        for (String directoryUrl : directoryUrlList) {
 
-        List<String> urlList = readLinesFromFile(DEFAULT_URL_LIST_PATH);
-        for (String url : urlList) {
+            List<String> pomUrlList =  PomSpider.findAllPomUrlInDirectory(directoryUrl);
             PomParser pomParser = new PomParser();
-            pomParser.parsePom(url);
+            for (String pomUrl : pomUrlList){
+                pomParser.parsePom(pomUrl);
+            }
         }
-
-
     }
+
 
 
     private static List<String> readLinesFromFile(String filePath) {
