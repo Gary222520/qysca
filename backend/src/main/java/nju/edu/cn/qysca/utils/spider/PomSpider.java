4c40416c5f8b71537b38e6ffc7561d57ad726f1e
jinqiqing <389459119@qq.com>
2024-01-11 15:37:55 +0800

    删除parser和spider废弃的代码

--- backend/src/main/java/nju/edu/cn/qysca/utils/spider/PomSpider.java
+++ backend/src/main/java/nju/edu/cn/qysca/utils/spider/PomSpider.java
@@ -43,26 +43,6 @@ public class PomSpider {
         return model;
     }
 
-
-//    /**
-//     * 通过url获取pom文件，存至Document对象中
-//     * @param pomUrl pom文件的url
-//     * @return org.jsoup.nodes.Document
-//     */
-//    public static Document getPomAsDocument(String pomUrl){
-//
-//        System.out.println("working on : " + pomUrl);
-//
-//        // 确认url存在并且为一个pom文件
-//        if (pomUrl == null || !pomUrl.endsWith(".pom")) {
-//            System.err.println("\"" + pomUrl + "\" is not a valid url or a pom file.");
-//            return null;
-//        }
-//
-//        // 以org.jsoup.nodes.Document返回其内容
-//        return (Document) UrlConnector.getDocumentByUrl(pomUrl);
-//    }
-
     /**
      * 在指定目录url下查找.pom文件
      *
