efe736f3a7e4f4ec49b3d98ce0289e6835aef02a
yuanjiale <1319091298@qq.com>
2024-01-16 13:21:21 +0800

    更新部分表结构

--- backend/src/main/java/nju/edu/cn/qysca/service/project/ProjectService.java
+++ backend/src/main/java/nju/edu/cn/qysca/service/project/ProjectService.java
@@ -1,5 +1,6 @@
 package nju.edu.cn.qysca.service.project;
 
+import nju.edu.cn.qysca.domain.project.ProjectInfoDO;
 import nju.edu.cn.qysca.domain.project.SaveProjectDTO;
 
 import java.util.List;
@@ -9,4 +10,11 @@ public interface ProjectService {
     Boolean saveProject(SaveProjectDTO saveProjectDTO);
 
     List<String> findAllDistinctProjectName();
+
+    /**
+     *  获取所有项目信息
+     * @return List<Project> 项目信息列表
+     */
+    List<ProjectInfoDO> getProjectList();
+
 }
