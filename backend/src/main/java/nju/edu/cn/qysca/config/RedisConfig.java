7189a30f3d4fd470ea37744fbeecf707a87e05d2
yuanjiale <1319091298@qq.com>
2024-03-04 21:01:49 +0800

    update: pg and redis

--- backend/src/main/java/nju/edu/cn/qysca/config/RedisConfig.java
+++ backend/src/main/java/nju/edu/cn/qysca/config/RedisConfig.java
@@ -1,21 +1,21 @@
-//package nju.edu.cn.qysca.config;
-//
-//import org.springframework.context.annotation.Bean;
-//import org.springframework.context.annotation.Configuration;
-//import org.springframework.data.redis.connection.RedisConnectionFactory;
-//import org.springframework.data.redis.core.RedisTemplate;
-//import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
-//import org.springframework.data.redis.serializer.StringRedisSerializer;
-//
-//@Configuration
-//public class RedisConfig {
-//    @Bean
-//    public RedisTemplate<String, Object> redisTemplate(RedisConnectionFactory redisConnectionFactory) {
-//        RedisTemplate<String, Object> redisTemplate = new RedisTemplate<>();
-//        StringRedisSerializer stringRedisSerializer = new StringRedisSerializer();
-//        redisTemplate.setConnectionFactory(redisConnectionFactory);
-//        redisTemplate.setKeySerializer(stringRedisSerializer);
-//        redisTemplate.setValueSerializer(new GenericJackson2JsonRedisSerializer());
-//        return redisTemplate;
-//    }
-//}
+package nju.edu.cn.qysca.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
+
+@Configuration
+public class RedisConfig {
+    @Bean
+    public RedisTemplate<String, Object> redisTemplate(RedisConnectionFactory redisConnectionFactory) {
+        RedisTemplate<String, Object> redisTemplate = new RedisTemplate<>();
+        StringRedisSerializer stringRedisSerializer = new StringRedisSerializer();
+        redisTemplate.setConnectionFactory(redisConnectionFactory);
+        redisTemplate.setKeySerializer(stringRedisSerializer);
+        redisTemplate.setValueSerializer(new GenericJackson2JsonRedisSerializer());
+        return redisTemplate;
+    }
+}
--- backend/src/main/java/nju/edu/cn/qysca/config/ThreadPoolConfig.java
+++ backend/src/main/java/nju/edu/cn/qysca/config/ThreadPoolConfig.java
@@ -1,34 +1,34 @@
-//package nju.edu.cn.qysca.config;
-//
-//import org.springframework.context.annotation.Bean;
-//import org.springframework.context.annotation.Configuration;
-//import org.springframework.core.task.TaskExecutor;
-//import org.springframework.scheduling.annotation.EnableAsync;
-//import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
-//
-//import java.util.concurrent.ThreadPoolExecutor;
-//
-//@EnableAsync
-//@Configuration
-//public class ThreadPoolConfig {
-//
-//    @Bean
-//    public TaskExecutor taskExecutor() {
-//        ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
-//        // 设置核心线程数
-//        executor.setCorePoolSize(50);
-//        // 设置最大线程数
-//        executor.setMaxPoolSize(200);
-//        // 设置队列容量
-//        executor.setQueueCapacity(200);
-//        // 设置线程活跃时间（秒）
-//        executor.setKeepAliveSeconds(800);
-//        // 设置默认线程名称
-//        executor.setThreadNamePrefix("Qysca-Async-");
-//        // 设置拒绝策略
-//        executor.setRejectedExecutionHandler(new ThreadPoolExecutor.CallerRunsPolicy());
-//        // 等待所有任务结束后再关闭线程池
-//        executor.setWaitForTasksToCompleteOnShutdown(true);
-//        return executor;
-//    }
-//}
+package nju.edu.cn.qysca.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.task.TaskExecutor;
+import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
+
+import java.util.concurrent.ThreadPoolExecutor;
+
+@EnableAsync
+@Configuration
+public class ThreadPoolConfig {
+
+    @Bean
+    public TaskExecutor taskExecutor() {
+        ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
+        // 设置核心线程数
+        executor.setCorePoolSize(50);
+        // 设置最大线程数
+        executor.setMaxPoolSize(200);
+        // 设置队列容量
+        executor.setQueueCapacity(200);
+        // 设置线程活跃时间（秒）
+        executor.setKeepAliveSeconds(800);
+        // 设置默认线程名称
+        executor.setThreadNamePrefix("Qysca-Async-");
+        // 设置拒绝策略
+        executor.setRejectedExecutionHandler(new ThreadPoolExecutor.CallerRunsPolicy());
+        // 等待所有任务结束后再关闭线程池
+        executor.setWaitForTasksToCompleteOnShutdown(true);
+        return executor;
+    }
+}
