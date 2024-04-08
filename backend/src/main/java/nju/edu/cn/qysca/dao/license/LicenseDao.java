9660999a769c54c53effe9e1a0159adf12c13ed3
huangbaojun <201250084@smail.nju.edu.cn>
2024-04-08 21:27:56 +0800

    多语言融合后端的稳定版本

--- backend/src/main/java/nju/edu/cn/qysca/dao/license/LicenseDao.java
+++ backend/src/main/java/nju/edu/cn/qysca/dao/license/LicenseDao.java
@@ -34,11 +34,23 @@ public interface LicenseDao extends JpaRepository<LicenseDO, String> {
     List<LicenseDO> findAllByNameLike(String name);
 
 
+    /**
+     * 查询应用的license
+     * @param licenses 应用的license信息
+     * @param pageable 分页信息
+     * @return Page<LicenseDO> 许可证信息
+     */
     @Query(value = "select * from plt_license where name in (:licenses) ORDER BY ARRAY_POSITION(ARRAY['high', 'medium', 'low'], risk_level), name asc",
             countQuery = "select count (*) from plt_license where name in (:licenses)",
             nativeQuery = true)
     Page<LicenseDO> getLicenseList(@Param("licenses") List<String> licenses, Pageable pageable);
 
+    /**
+     * 许可证库界面的查询
+     * @param name 许可证名称
+     * @param pageable 分页信息
+     * @return Page<LicenseDO> 许可证信息
+     */
     @Query(value = "select * from plt_license where (:name = '' or :name = name) ORDER BY ARRAY_POSITION(ARRAY['high', 'medium', 'low'], risk_level), name asc",
                countQuery = "select count (*) from plt_license where (:name = '' or :name = name)",
                nativeQuery = true)
