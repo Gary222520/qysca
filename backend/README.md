54b4c6b42aa5f25ad096ca1079617fe67f1aa9e2
jinqiqing <389459119@qq.com>
2024-04-18 20:08:56 +0800

    feat: java爬虫分支

--- backend/README.md
+++ backend/README.md
@@ -1,3 +1,5 @@
 # SCA backend
 
 采用springboot MVC架构，整合jpa和swagger-ui辅助开发。
+
+该分支 'feat_spider_run_java' 启动后将自动运行java批量爬取，占用9091端口。
--- backend/src/main/java/nju/edu/cn/qysca/MyApplication.java
+++ backend/src/main/java/nju/edu/cn/qysca/MyApplication.java
@@ -1,13 +1,27 @@
 package nju.edu.cn.qysca;
 
+import nju.edu.cn.qysca.run.JavaSpiderRun;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import javax.annotation.PostConstruct;
+
 @SpringBootApplication
 public class MyApplication {
+
+    @Autowired
+    private JavaSpiderRun javaSpiderRun;
 
     public static void main(String[] args) {
         SpringApplication.run(MyApplication.class, args);
     }
 
+    /**
+     * 启动后运行java爬虫
+     */
+    @PostConstruct
+    private void run(){
+        javaSpiderRun.executeSpiderTask();
+    }
 }
