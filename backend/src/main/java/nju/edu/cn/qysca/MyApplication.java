b0a920645bed29c2a5ff6d731cbf213f429784e3
jinqiqing <389459119@qq.com>
2024-04-18 20:22:47 +0800

    feat: js爬虫分支

--- backend/src/main/java/nju/edu/cn/qysca/MyApplication.java
+++ backend/src/main/java/nju/edu/cn/qysca/MyApplication.java
@@ -1,13 +1,27 @@
 package nju.edu.cn.qysca;
 
+import nju.edu.cn.qysca.run.JsSpiderRun;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import javax.annotation.PostConstruct;
+
 @SpringBootApplication
 public class MyApplication {
+
+    @Autowired
+    private JsSpiderRun jsSpiderRun;
 
     public static void main(String[] args) {
         SpringApplication.run(MyApplication.class, args);
     }
 
+    /**
+     * 启动后运行js爬虫
+     */
+    @PostConstruct
+    private void run(){
+        jsSpiderRun.executeSpiderTask();
+    }
 }
