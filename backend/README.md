22f0f1bf687f1758a19cd83d19672a09f8778a9e
jinqiqing <389459119@qq.com>
2024-04-18 20:20:26 +0800

    feat: python爬虫分支

--- backend/README.md
+++ backend/README.md
@@ -1,3 +1,5 @@
 # SCA backend
 
 采用springboot MVC架构，整合jpa和swagger-ui辅助开发。
+
+该分支 'feat_spider_run_python' 启动后将自动运行python批量爬取，占用9092端口。
--- backend/src/main/java/nju/edu/cn/qysca/MyApplication.java
+++ backend/src/main/java/nju/edu/cn/qysca/MyApplication.java
@@ -1,13 +1,27 @@
 package nju.edu.cn.qysca;
 
+import nju.edu.cn.qysca.run.PythonSpiderRun;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import javax.annotation.PostConstruct;
+
 @SpringBootApplication
 public class MyApplication {
+
+    @Autowired
+    private PythonSpiderRun pythonSpiderRun;
 
     public static void main(String[] args) {
         SpringApplication.run(MyApplication.class, args);
     }
 
+    /**
+     * 启动后运行python爬虫
+     */
+    @PostConstruct
+    private void run(){
+        pythonSpiderRun.executeSpiderTask();
+    }
 }
